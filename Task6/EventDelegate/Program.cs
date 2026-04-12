ff785dea83854231bf1160cbff2016f784ecb0f0
Vigneshwaran B <vigneshsobalamurugan2005@gmail.com>
2026-04-12 15:43:55 +0530

    Task6

---
+++ Task6/EventDelegate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class BankAccount{
+    private int balance;
+
+    public delegate void LowBalanceNotify(int balance);
+    public event LowBalanceNotify LowBalance;
+
+    public BankAccount(int balance){
+        this.balance=balance;
+    }
+
+    public void Withdraw(int amount){
+        if(balance-amount<=0){
+            Console.WriteLine("Insufficient Balance. Withdrawal Failed.");
+            return;
+        }
+        balance -= amount;
+        Console.WriteLine("Withdrawn: ₹" + amount);
+        Console.WriteLine("Current Balance: ₹" + balance);
+
+        if (balance < 1000){
+            OnLowBalance(balance);
+        }
+    }
+      
+    protected void OnLowBalance(int balance){
+        LowBalance?.Invoke(balance);
+    }
+}
+
+class Program{
+    static void SendSMS(int balance){
+        Console.WriteLine("SMS Alert: Your balance is low: ₹" + balance);
+    }
+
+    static void SendEmail(int balance){
+        Console.WriteLine("Email Alert: Please maintain minimum balance. Current: ₹" + balance);
+    }
+
+    static void ShowMobileNotification(int balance){
+        Console.WriteLine("App Notification: Low balance warning!");
+    }
+
+    static void Main(){
+        BankAccount account= new BankAccount(3000);
+
+        account.LowBalance+=SendSMS;
+        account.LowBalance+=SendEmail;
+        account.LowBalance+=ShowMobileNotification;
+
+        account.Withdraw(700);
+        account.Withdraw(600);
+        account.Withdraw(800);
+    }
+}
---
+++ Task6/EventDelegate/README.md
@@ -0,0 +1,16 @@
+# Delegates, Events, and Basic Event Handling
+# Objective & Requirements:
+- Build a console-based event-driven application (e.g., a counter that
+triggers an event at a threshold).
+- Define a delegate and an event that fires when a counter reaches a
+specific value.
+- Create multiple event handler methods that perform actions when the
+event is raised.
+- In your main loop, increment the counter and raise the event when
+appropriate.
+- Demonstrate how events can decouple the producer and consumer
+logic.
+
+# Result
+
+![Output](image.png)
