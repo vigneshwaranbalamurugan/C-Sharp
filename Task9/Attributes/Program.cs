00a5f35c60b99c24d009791a063d65e5fa9a5fc2
Vigneshwaran B <vigneshsobalamurugan2005@gmail.com>
2026-04-13 22:50:56 +0530

    Task9

---
+++ Task9/Attributes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class LoginAttribute : Attribute{
+}
+
+public class LoginTasks{
+    [LoginAttribute]
+    public void SendOTP(){
+        Console.WriteLine("OTP sent to your mobile number");
+    }
+
+    [LoginAttribute]
+    public void VerifyOTP(){
+        Console.WriteLine("OTP verified successfully");
+    }
+
+    [LoginAttribute]
+    public void ShowWelcomeMessage(){
+        Console.WriteLine("Login successful. Welcome user!");
+    }
+
+    public void ErrorMessage(){
+        Console.WriteLine("Wrong OTP. Please try again.");
+    }
+}
+
+class Program{
+    static void Main(){
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        foreach (Type type in assembly.GetTypes()){
+            foreach (MethodInfo method in type.GetMethods()){
+                if (method.GetCustomAttribute(typeof(LoginAttribute)) != null){
+                    object obj = Activator.CreateInstance(type);
+                    Console.WriteLine($"Running: {method.Name}");
+                    method.Invoke(obj, null);
+                }
+            }
+        }
+
+        Console.WriteLine("\nProcess completed");
+    }
+}
---
+++ Task9/Attributes/README.md
@@ -0,0 +1,14 @@
+# Reflection and Custom Attributes
+# Objective & Requirements:
+1. Build an application that discovers and executes methods based on
+custom attributes.
+2. Define a custom attribute (e.g., [Runnable] ).
+3. Create several classes with methods decorated with the [Runnable]
+attribute.
+4. Use reflection to scan the current assembly for methods marked with
+[Runnable] .
+5. Invoke the discovered methods dynamically and display their outputs.
+
+# Output
+
+![Output](image.png)
